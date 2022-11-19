47867f8ae279511348b3e2c6433cec9017094162
Adwin Prajisha S L <112170301+ADWIN-PRAJISHA-S-L@users.noreply.github.com>
2022-11-19 15:04:27 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 3/Alert.py
@@ -0,0 +1,19 @@
+# import required module
+from playsound import playsound
+
+# for playing note.wav file
+playsound('./alarm-sound.mp3')
+print('playing sound using playsound')
+#7200138999if pred == 0:
+     playsound('./alarm-sound.mp3')
+     print('playing sound using playsound')
+else:
+    print('no fire')
+
+
+
+if pred == 0:
+     playsound('./alarm-sound.mp3')
+     print('playing sound using playsound')
+else:
+    print('no fire')
